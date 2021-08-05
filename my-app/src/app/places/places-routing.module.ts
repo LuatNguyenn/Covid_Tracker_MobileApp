ba28230c399ebc9e10f7cd519a8cb6ca71e3330a
LuatNguyenn <luat.nguyen@edu.sait.ca>
2021-08-05 10:15:33 -0600

    routing added

--- my-app/src/app/places/places-routing.module.ts
+++ my-app/src/app/places/places-routing.module.ts
@@ -5,21 +5,38 @@ import { PlacesPage } from './places.page';
 
 const routes: Routes = [
   {
-    path: '',
-    component: PlacesPage
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      {
+        path: 'discover', children: [
+          { path: '', loadChildren: () => import('./discover/discover.module').then(m => m.DiscoverPageModule) }
+        ]
+      },
+
+      {
+        path: ':placeId',
+        loadChildren: () => import('./discover/place-details/place-details.module').then(m => m.PlaceDetailsPageModule)
+      },
+      //in case navigate to /places/tabs
+      { path: '', redirectTo: '/places/tabs/discover', pathMatch: 'full' }
+    ]
   },
   {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
+    //need to match tab name
+    path: 'offers', children: [
+      { path: '', loadChildren: () => import('./offers/offers.module').then(m => m.OffersPageModule) },
+      { path: 'new', loadChildren: () => import('./offers/new-offer/new-offer.module').then(m => m.NewOfferPageModule) }, //hard code route first
+      { path: 'edit/:placeId', loadChildren: () => import('./offers/edit-offer/edit-offer.module').then(m => m.EditOfferPageModule) },
+      { path: ':placeId', loadChildren: () => import('./offers/place-booking/place-booking.module').then(m => m.PlaceBookingPageModule) },
+    ]
   },
-  {
-    path: 'offers',
-    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
-  }
+  { path: '', redirectTo: 'tabs/discover', pathMatch: 'full' } //pathMatch to ensure correct path places/tabs...
+
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class PlacesPageRoutingModule {}
+export class PlacesPageRoutingModule { }
--- my-app/src/app/places/places.module.ts
+++ my-app/src/app/places/places.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -13,7 +14,8 @@ import { PlacesPage } from './places.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    PlacesPageRoutingModule
+    PlacesPageRoutingModule,
+    // RouterModule.forChild(PlacesPageRoutingModule)
   ],
   declarations: [PlacesPage]
 })
